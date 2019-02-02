79986252ab0ccc11217c388e3ab3d91e9585bf7b
Jasmin Parent <guildwarsguy12@gmail.com>
2019-02-02 16:18:49 -0500

    Update Imc.scala

--- Imc.scala
+++ Imc.scala
@@ -1,43 +1,114 @@
-import java.util.Scanner
+import scala.io.Source
 
-object Imc extends App {
-  val poids = List("Poids insuffisant", "Poids normal", "Excès de poids", "Obésité, classe I", "Obésité, classe II", "Obésité, classe III")
-  val risques = List("Accru", "Moindre", "Accru", "Élevé", "Très élevé", "Extrêmement élevé")
-  val imcs = List(0, 18.5, 25, 30, 35, 39.9)
+object lists extends App {
 
-  def getUserInputs: (Double, Double) = {
-    val lecteur = new Scanner(System.in)
-    var poid: Double = 0
-    var taille: Double = 0
+  val myList2 = List("Introduction à la Programmation",
+    "Pratique de la Programmation",
+    "Structure de données",
+    "Principe de la Programmation",
+    "Algorithmie",
+    "Langages de Programmation")
 
-    try {
-      while (poid == 0 || taille == 0) {
-        println("Entrez votre poid: ")
-        poid = lecteur.nextDouble()
-        println("Entrez votre taille: ")
-        taille = lecteur.nextDouble()
-      }
-    } catch { case _: Exception => getUserInputs }
-    (poid, taille)
+  //=============================================================================================
+  // Exer 1, find sum
+  // functional
+  val myList = List(15, 39, 22, 98, 37, 19, 5)
+  // method 1
+  val sum1: Int = myList.sum // 235
+  // method 2
+  val sum2: Int = myList.reduce((a, b) => a + b) // 235
+
+  //=============================================================================================
+  // Exer 2
+  // Combien d'éléments contiennent le mot "Programmation"
+  val res1 = myList2.count(p => p contains "Programmation") // 4
+
+  // Combien d'éléments ne contiennent pas le mot "Programmation"
+  val res2 = myList2.count(p => !(p contains "Programmation")) // 2
+
+  // Déterminer s’il y’a au moins un élément qui ne contient pas le mot "Programmation"
+  val res3 = myList2.count(p => !(p contains "Programmation")) >= 1 // true
+
+  // Déterminer si la lettre o (maj ou min) est présente dans tous les éléments de la collection
+  val res4 = myList2.count(p => p.toLowerCase contains "o") == myList2.length // true
+
+  // Déterminer s'il y'a au moins un élément qui contient le mot "Programmation"
+  val res5 = myList2.count(p => p contains "Programmation") >= 1 // true
+
+  println(res1, res2, res3, res4, res5)
+
+  //=============================================================================================
+  // Exer 3
+  // Elements contenant un espace
+  val res6 = myList2.filter(e => e contains " ")
+
+  // Sans espaces
+  val res7 = myList2.filter(e => !(e contains " "))
+
+  println(res6)
+  println(res7)
+
+  //=============================================================================================
+  // Exer 4
+  // 1. lire le fichier, afficher le temps pris à le lire et le nombre de mots dans le fichier
+  def lireFichier(file_path: String): Array[String] = {
+    val temps_depart = System.currentTimeMillis()
+    val F = Source.fromFile(file_path)
+    var tab = new Array[String](0)
+
+    for (ligne <- F.getLines()) {
+      tab = tab :+ ligne.mkString
+    }
+
+    val len = tab.length
+    val duree = System.currentTimeMillis() - temps_depart
+    println(s"Nombre de mots: $len. Duree de l'execution: $duree ms.")
+
+    tab
   }
 
-  def calculImc(poid: Double, taille: Double): Double = poid / Math.pow(taille, 2)
+  val mots = lireFichier("C:\\Users\\guild\\IdeaProjects\\IMC\\src\\english_words.txt")
 
-  def classifyImc(imc: Double): (String, String) = {
-    var i: Int = -1
-    for (j <- imcs.indices) if (imc > imcs(j)) i += 1
-    (poids(i), risques(i))
+
+  // 2. Afficher tout les mots contenat "issis"
+  mots
+    .filter(mot => mot contains "issis")
+    .foreach(mot => println(mot))
+
+
+  // 3. Mot de longueur superieur ou egale a 18
+  mots
+    .filter(mot => mot.length >= 18)
+    .foreach(mot => println(mot))
+
+
+  // 4. Mots ne contenant pas de voyelles
+  def contains_no_vowel(mot: String): Boolean = {
+    val voyelles = List('a', 'e', 'i', 'o', 'u', 'y')
+    var bool = true
+    voyelles.foreach(v => {
+      if (mot contains v) bool = false
+    })
+    bool
   }
 
-  def createMessage(imc: Double, poid: String, risque: String): String = s"Votre IMC: $imc, $poid, risque: $risque"
+  val resp = mots filter contains_no_vowel
+  resp foreach println
 
-  def main(): Unit = {
-    val (poid, taille) = getUserInputs
-    val imc = calculImc(poid, taille)
-    val (poid_cat, risque_cat) = classifyImc(imc)
-    val msg = createMessage(imc, poid_cat, risque_cat)
-    println(msg)
-  }
 
-  main()
+
+  //=============================================================================================
+  // Exer 5
+  // 1. Obtenir la collection transformée qui contient la longueur de chacun des éléments de la collection d'origine
+  val res8 = myList2.map(el => el.length)
+  println(res8)
+
+  // 2. Obtenir la collection transformée qui contient les éléments en majuscule de chaque élément de la collection d'origine
+  val res9 = myList2.map(el => el.toUpperCase)
+  println(res9)
+
+  // 3. Obtenir la collection transformée qui a chaque élément préfixé par le mot "Livre:" au début de chaque élément de la collection d'origine
+  val res10 = myList2.map(el => "Livre: " + el)
+  println(res10)
 }
+
